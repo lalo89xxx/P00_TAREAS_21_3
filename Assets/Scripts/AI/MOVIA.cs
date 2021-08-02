8312f6be5f402ff888c2e1e9e977f114ff5c6e16
lalo89xxx <eduardotorresf89@gmail.com>
2021-08-02 01:53:20 -0500

    Se creo la inteligencia basica del enemigo

--- Assets/Scripts/AI/MOVIA.cs
+++ Assets/Scripts/AI/MOVIA.cs
@@ -4,15 +4,74 @@ using UnityEngine;
 
 public class MOVIA : MonoBehaviour
 {
+    public Transform Target;
+
+    public float Velocidad;
+
+    public Animator AnimEnemigo;
+
+    public float DistanciaExacta;
+
+    
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        AnimEnemigo = GetComponent <Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        //Formula:
+        //Direccion = punto de llagada - punto de origen;
+        Vector3 direccion = Target.position - transform.position;
+        Debug.Log(direccion.magnitude);
+
+        transform.LookAt(Target);
+
+        if(direccion.magnitude > DistanciaExacta)
+        {
+             Move(direccion);
+             ActivarAnimacionBool("RUN",true);
+        }
+
+       else if(direccion.magnitude <= DistanciaExacta)
+       {
+           PlayerAnimation("Quad Punch");
+
+       }
+       
+       else
+       {
+            PlayerAnimation("Idle"); 
+            ActivarAnimacionBool("RUN",false);
+
+       }
+       
+
+
+    }
+
+    private void Move(Vector3 direccion)
+    {
         
+        
+     this.transform.Translate(direccion.normalized * Velocidad * Time.deltaTime,Space.World);
+             
+        
+
+    }
+
+     private void ActivarAnimacionBool(string NombreAnimacion, bool Valor)
+    {
+
+        AnimEnemigo.SetBool(NombreAnimacion, Valor);
+    }
+
+    private void PlayerAnimation(string NombreClip)
+    {
+        AnimEnemigo.Play(NombreClip);
     }
 }
